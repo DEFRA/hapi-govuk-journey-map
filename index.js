aba0e8c9b2db8a666a9ea648c35666c1c01a75fd
ben <ben@surgison.net>
2020-04-06 11:16:34 +0100

    Expose isJourneyRoute function fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const pkg = require('./package')
-const { register, setQueryData, getQueryData, getCurrent, getRoute, getMap } = require('./lib/journey-map')
+const { register, setQueryData, getQueryData, getCurrent, getRoute, getMap, isJourneyRoute } = require('./lib/journey-map')
 
 exports.plugin = {
   name: pkg.name,
@@ -13,3 +13,4 @@ exports.getQueryData = getQueryData
 exports.getCurrent = getCurrent
 exports.getRoute = getRoute
 exports.getMap = getMap
+exports.isJourneyRoute = isJourneyRoute
